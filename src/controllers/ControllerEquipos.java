0fb56e7410aba83112792ad3db7f0cb7eb395649
antonio bruna <joseantonio.bruna.delcarmen@iestubalcain.net>
2019-02-25 17:49:31 +0100

    clase de equipos

--- src/controllers/ControllerEquipos.java
+++ src/controllers/ControllerEquipos.java
@@ -1,5 +1,32 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import daos.EquiposDaoMySQLImp;
+import daos.JugadorDaoMongoDBImp;
+import daos.JugadorDaoMySQLImp;
+import daos.EquiposDaoMongoDBImp;
+
+import idaos.EquiposDao;
+import idaos.JugadorDao;
+import modelos.Equipos;
+import modelos.Jugador;
+import vistas.ViewEquipos;
+
 public class ControllerEquipos {
 
+	private ViewEquipos vista = new ViewEquipos();
+	
+	
+	public ControllerEquipos() {
+	}
+	
+	public void create(Jugador jugador) {
+		EquiposDao dao = new EquiposDaoMySQLImp();
+		EquiposDao daoMongo = new EquiposDaoMongoDBImp();
+		
+	    //dao.create(jugador);
+		daoMongo.create(equipos);
+	}
 }
